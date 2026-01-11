2dfcff45edf7e055de5dcc4040374a89531b8cfa
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:53:15 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "postgresql_flexible_server_databases" {
+  description = "All postgresql_flexible_server_database resources"
+  value       = azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases
+}
+
