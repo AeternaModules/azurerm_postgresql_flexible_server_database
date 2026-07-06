346bef4e12fe027701a7a9f54ec5549984a15dde
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "postgresql_flexible_server_databases" {
   description = "All postgresql_flexible_server_database resources"
   value       = azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases
 }
+output "postgresql_flexible_server_databases_charset" {
+  description = "List of charset values across all postgresql_flexible_server_databases"
+  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.charset]
+}
+output "postgresql_flexible_server_databases_collation" {
+  description = "List of collation values across all postgresql_flexible_server_databases"
+  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.collation]
+}
+output "postgresql_flexible_server_databases_name" {
+  description = "List of name values across all postgresql_flexible_server_databases"
+  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.name]
+}
+output "postgresql_flexible_server_databases_server_id" {
+  description = "List of server_id values across all postgresql_flexible_server_databases"
+  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.server_id]
+}
 
