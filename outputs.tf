48ae6b7c9717161701547775f969d78f8375e6ba
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "postgresql_flexible_server_databases" {
-  description = "All postgresql_flexible_server_database resources"
-  value       = azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases
-}
 output "postgresql_flexible_server_databases_charset" {
-  description = "List of charset values across all postgresql_flexible_server_databases"
-  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.charset]
+  description = "Map of charset values across all postgresql_flexible_server_databases, keyed the same as var.postgresql_flexible_server_databases"
+  value       = { for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : k => v.charset }
 }
 output "postgresql_flexible_server_databases_collation" {
-  description = "List of collation values across all postgresql_flexible_server_databases"
-  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.collation]
+  description = "Map of collation values across all postgresql_flexible_server_databases, keyed the same as var.postgresql_flexible_server_databases"
+  value       = { for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : k => v.collation }
 }
 output "postgresql_flexible_server_databases_name" {
-  description = "List of name values across all postgresql_flexible_server_databases"
-  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.name]
+  description = "Map of name values across all postgresql_flexible_server_databases, keyed the same as var.postgresql_flexible_server_databases"
+  value       = { for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : k => v.name }
 }
 output "postgresql_flexible_server_databases_server_id" {
-  description = "List of server_id values across all postgresql_flexible_server_databases"
-  value       = [for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : v.server_id]
+  description = "Map of server_id values across all postgresql_flexible_server_databases, keyed the same as var.postgresql_flexible_server_databases"
+  value       = { for k, v in azurerm_postgresql_flexible_server_database.postgresql_flexible_server_databases : k => v.server_id }
 }
 
